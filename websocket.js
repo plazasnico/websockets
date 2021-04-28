0805157b22329130c5a2c4fd4f8f491638292a71
Nicolas Plazas <plazasnico@hotmail.com>
2021-04-27 22:51:22 -0500

    Updating the process of websocket client and remove the websocket server with webserver over http

--- websocket.js
+++ websocket.js
@@ -1,13 +1,24 @@
-const WebSocket = require('ws');
+// const WebSocket = require('ws');
 
-const wss = new WebSocket.Server({ port: process.env.PORT || 8080 });
+// const wss = new WebSocket.Server({ port: process.env.PORT || 8080 });
 
-wss.on('connection', function connection(ws) {
-    console.log("Usuario conectado");
+// wss.on('connection', function connection(ws) {
+//     console.log("Usuario conectado");
 
-    ws.on('message', function incoming(message) {
-        console.log('received: %s %s', message, new Date());
-        ws.send('Recibido: ' + message.toString());
-    });
+//     ws.on('message', function incoming(message) {
+//         console.log('received: %s %s', message, new Date());
+//         ws.send('Recibido: ' + message.toString());
+//     });
     
-});
+// });
+
+const http = require('http');
+const fs = require('fs');
+
+http.createServer((req,res)=>{
+    fs.readFile('websocket-client.html', function(error, data){
+        res.writeHead(200, {'Content-Type':'text/html'});
+        res.write(data);
+        res.end();
+    });
+}).listen(process.env.PORT||8080);
